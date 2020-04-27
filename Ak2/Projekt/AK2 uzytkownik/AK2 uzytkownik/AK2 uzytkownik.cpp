c7969eca630b9e7268227335e3a1d4813df654ef
n3shi <neshiteam@gmail.com>
2020-04-27 10:10:18 +0200

    Podpięto zapis, odczyt wyników. Dodatkowo dołożono funkcję mierzącą czas, aktualizacja wszystkich możliwych przypadków i odpowiedzi, zabiegi pielęgnacyjne wyszczuplające kod

--- Ak2/Projekt/AK2 uzytkownik/AK2 uzytkownik/AK2 uzytkownik.cpp
+++ Ak2/Projekt/AK2 uzytkownik/AK2 uzytkownik/AK2 uzytkownik.cpp
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include "SerialPort.h"
 
+
 using std::cout;
 using std::cin;
 using namespace std;
@@ -24,6 +25,41 @@ using namespace chrono;
 
 int liczba; //podana liczba (nwm czy liczba bedzie taka, czy nie bedzie w formacie string, zobaczymy)
 bool wynik; //czy jest pierwsza czy nie
+string czasWykonania; //jak szybko udalo sie wyslac liczbe do Arduino, policzyc w arduino i odebrac wynik
+bool canSave = false;
+
+//
+//
+// MIERZENIE CZASU
+//
+//
+
+class Czas {
+public:
+	high_resolution_clock::time_point czasPoczatkowy;
+	high_resolution_clock::time_point czasKoncowy;
+
+	void czasStart();
+	void czasStop();
+	long czasWykonania();
+};
+
+void Czas::czasStart() // rozpoczecie mierzenia czasu
+{
+	czasPoczatkowy = high_resolution_clock::now();
+}
+
+void Czas::czasStop() // zakonczenie mierzenia czasu
+{
+	czasKoncowy = high_resolution_clock::now();
+}
+
+long Czas::czasWykonania() // zwraca czas pomiedzy funkcjami czasPoczatkowy i czas Koncowy
+{
+
+	return duration_cast<milliseconds>(Czas::czasKoncowy - Czas::czasPoczatkowy).count();
+
+}
 
 
 
@@ -47,45 +83,59 @@ void MenuGlowne()
 	cout << "   2. Wczytanie wszystkich wynikow\n";
 	cout << "   0. Wyjscie z aplikacji\n";
 }
-
-void ZapiszWynikiDoPliku(int liczba, bool wyniczek)
+void MenuWynikow()
 {
-	if (wyniczek)
-	{
-		try {
-			fstream plik; // nazwa pliku do ktorego podajemy kolejne czasy dodawan
+	Naglowek();
 
-			plik.open("LiczbyPierwsze.txt", ios::out | ios::app);
-			if (plik.good() == true)
-			{
-				plik << liczba << "\n";
+	cout << "	M E N U   W Y N I K O W\n\n";
+	cout << "   1. Znalezione liczby pierwsze\n";
+	cout << "   2. Znalezione liczby nie pierwsze\n";
+	cout << "   0. Wyjscie z aplikacji\n";
+}
+
+void ZapiszWynikiDoPliku(int liczba, bool wyniczek, string czas)
+{
+	if (canSave)
+	{
+
+		if (wyniczek)
+		{
+			try {
+				fstream plik; // nazwa pliku do ktorego podajemy kolejne czasy dodawan
+
+				plik.open("LiczbyPierwsze.txt", ios::out | ios::app);
+				if (plik.good() == true)
+				{
+					plik << "\n" << liczba << " jest liczba pierwsza. Czas obliczen: " << czas;
+					plik.close();
+				}
 				plik.close();
+				cout << "Wynik zostal zapisany poprawnie";
 			}
-			plik.close();
-			cout << "Wynik zostal zapisany poprawnie";
+			catch (exception e) {
+				cout << "Wynik nie zostal zapisany poprawnie";
+			}
 		}
-		catch (exception e) {
-			cout << "Wynik nie zostal zapisany poprawnie";
+		else
+		{
+			try {
+				fstream plik; // nazwa pliku do ktorego podajemy kolejne czasy dodawan
+
+				plik.open("LiczbyNiepierwsze.txt", ios::out | ios::app);
+				if (plik.good() == true)
+				{
+					plik << "\n" << liczba << " nie jest liczba pierwsza. Czas obliczen: " << czas;
+					plik.close();
+				}
+				plik.close();
+				cout << "Wynik zostal zapisany poprawnie";
+			}
+			catch (exception e) {
+				cout << "Wynik nie zostal zapisany poprawnie";
+			}
 		}
 	}
-	else
-	{
-		try {
-			fstream plik; // nazwa pliku do ktorego podajemy kolejne czasy dodawan
-
-			plik.open("LiczbyNiepierwsze.txt", ios::out | ios::app);
-			if (plik.good() == true)
-			{
-				plik << liczba << "\n";
-				plik.close();
-			}
-			plik.close();
-			cout << "Wynik zostal zapisany poprawnie";
-		}
-		catch (exception e) {
-			cout << "Wynik nie zostal zapisany poprawnie";
-		}
-	}
+	else cout << "\n Nie zapisano wyniku (brak wyniku)";
 }
 
 int algorytmPierwszosci(string liczba)
@@ -102,70 +152,44 @@ int algorytmPierwszosci(string liczba)
 	char incoming[MAX_DATA_LENGTH];
 
 	
-	SerialPort arduino(tab[i]);
-	
-	if (arduino.isConnected())
-	{
-		cout << "Polaczono sie na porcie: " << tab[i] << "\n\n";
-	}
-	else
-	{
-		cout << "Nie udalo sie polaczyc na procie: " << tab[i] << "\n\n";
-		i++;
 
+	for (int i = 0; i < 3; i++)
+	{
 		SerialPort arduino(tab[i]);
-		if (arduino.isConnected()) cout << "Polaczono sie na porcie: " << tab[i] << "\n\n";
-		else
+		canSave = false;
+		if (arduino.isConnected())
 		{
-			cout << "Nie udalo sie polaczyc na procie: " << tab[i] << "\n\n";
-			i++;
+			cout << "Polaczono sie na porcie: " << tab[i] << "\n\n";
 
-			SerialPort arduino(tab[i]);
-
-
-		}
-		if (arduino.isConnected()) cout << "Polaczono sie na porcie: " << tab[i] << "\n\n";
-		else
-		{
-			cout << "Nie udalo sie polaczyc na procie: " << tab[i] << "\n\n";
-			i++;
-
-			SerialPort arduino(tab[i]);
-
-			if (arduino.isConnected()) cout << "Polaczono sie na porcie: " << tab[i] << "\n\n";
-			else
+			while (arduino.isConnected()) //wykonuj dopoki arduino jest podlaczone
 			{
-				cout << "Nie znaleziono arduino na zadnym porcie, sprawdz poolaczenie i sprobuj ponownie";
-				liczba = _getch();
-				return 0;
+				string data; //to co wysylamy do arduino
+				data = liczba;
+				char* charArray = new char[data.size() + 1];
+				copy(data.begin(), data.end(), charArray);
+				charArray[data.size()] = '\n';
+
+				cout << "Pytanie do arduino: czy liczba " + liczba +
+					" jest liczba pierwsza?\n(odpowiedz od arduino bedzie taka: 1 - tak    2 - nie)";
+				arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
+				arduino.readSerialPort(output, MAX_DATA_LENGTH);
+
+				canSave = true;
+				cout << "Odpowiedz arduino: ";
+				cout << output;
+
+				delete[] charArray;
+				int tempx = atoi(output);
+
+				return tempx;
 			}
 
+			break;
 		}
-
-
-	}
-	
-
-	while (arduino.isConnected()) //wykonuj dopoki arduino jest podlaczone
-	{
-		string data; //to co wysylamy do arduino
-		data = liczba;
-		char* charArray = new char[data.size() + 1];
-		copy(data.begin(), data.end(), charArray);
-		charArray[data.size()] = '\n';
-
-		cout << "Pytanie do arduino: czy liczba " + liczba + 
-			" jest liczba pierwsza?\n1 - tak    2 - nie";
-		arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
-		arduino.readSerialPort(output, MAX_DATA_LENGTH);
-
-		cout << "Odpowiedz arduino: ";
-		cout << output;
-
-		delete[] charArray;
-		int tempx = atoi(output);
-
-		return tempx;
+		else
+		{
+			cout << "Nie udalo sie polaczyc na procie: " << tab[i] << "\n\n";
+		}
 	}
 }
 
@@ -186,11 +210,12 @@ string SzukajPierwszosci(int liczba)
 	if (tempLiczba == 1)
 	{
 		temp += " jest liczba pierwsza";
-		return temp;
+		wynik = true;
 	}
 	else if (tempLiczba == 2)
 	{
 		temp += " nie jest liczba pierwsza";
+		wynik = false;
 	}
 	else temp = "Odczytano cos innego";
 	return temp;
@@ -227,13 +252,22 @@ void ZnajdzLPierw()
 	do {
 		if (x == 1)
 		{
+			Czas time;
 			Naglowek();
 			WczytajLiczbe();
 
 			cout << "Rozpoczynam sprawdzanie pierwszosci podanej liczby: " << liczba << endl;
 
-			string wynik = SzukajPierwszosci(liczba);
-			cout << wynik;
+
+			time.czasStart();
+			string odpowiedz = SzukajPierwszosci(liczba);
+			time.czasStop();
+
+			czasWykonania = time.czasWykonania() * 0.001;
+			czasWykonania += " s";
+			cout << odpowiedz;
+
+			ZapiszWynikiDoPliku(liczba, wynik, czasWykonania);
 
 		cout << "\n\nCzy chcesz powtorzyc szukanie liczby? \n 1 - powtorz\n 0 - wyjdz" << endl;
 		cin >> x;
@@ -241,10 +275,55 @@ void ZnajdzLPierw()
 	} while (x == 1); //tu sie konczy doWhile
 }
 
+
+void Wyniki(string nazwa)
+{
+	fstream uchwyt; //obiekt typu fstream (uchwyt do pliku)
+
+	uchwyt.open(nazwa); //otwieramy plik: plik.txt (plik - nazwa pliku, txt - rozszerzenie)
+	string linia;
+
+	if(uchwyt.good())
+	while (!uchwyt.eof())
+	{
+		getline(uchwyt, linia); //pobierz linijkę
+		cout << linia << endl; //wypisz na ekranie
+	 }
+	uchwyt.close(); //zamykamy plik
+}
+
 void WczytajWyniki()
 {
+	int x = 1;
+	do {
+		if (x == 1)
+		{
+			MenuWynikow();
+			string nazwaPliku;
+			int temp = 0;
+			cout << "\n\nKtore wyniki wyswietlic?" << endl;
+			cin >> temp;
+			if (temp == 1)
+			{
+				cout << "\n\nWyswietlam znalezione liczby pierwsze:\n\n";
+				nazwaPliku = "liczbyPierwsze.txt";
+				Wyniki(nazwaPliku);
 
+			}
+			else if (temp == 2)
+			{
+				cout << "\n\nWyswietlam znalezione liczby nie pierwsze:\n\n";
+				nazwaPliku = "liczbyNiepierwsze.txt";
+				Wyniki(nazwaPliku);
+			}
+			else cout << "\n\nPodano niepoprawna wartosc";
+
+			cout << "\n\nCzy chcesz powtorzyc wyswietlanie? \n 1 - powtorz\n 0 - wyjdz" << endl;
+			cin >> x;
+		}
+	} while (x == 1); //tu sie konczy doWhile
 }
+
 
 
 
